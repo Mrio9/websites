f5112f13a91e1643283f3dcbb1ff7dfa9ae27565
Mrio9 <79405648+Mrio9@users.noreply.github.com>
2023-02-27 00:00:03 +0530

    a
    a

--- Portfolio2/main.js
+++ Portfolio2/main.js
@@ -13,3 +13,10 @@ buttons.forEach(button => {
         delete activeSlide.dataset.active
     })
 })
+var sidemenu = document.getElementById("sidemenu");
+        function openmenu(){
+            sidemenu.style.right = "0";
+        }
+        function closemenu(){
+            sidemenu.style.right = "-200px";
+        }
